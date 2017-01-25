d19d75b89b2d1a5c2d8fe20b80cd6760e116a280
Karla Aguilar <kn.aguilar7@gmail.com>
2017-01-24 21:47:02 -0500

    bug: missing items in url

--- geolocating.js
+++ geolocating.js
@@ -8,9 +8,8 @@ $(document).ready(function() {
     if ("geolocation" in navigator) {
       console.log("GeoLocation available");
       navigator.geolocation.getCurrentPosition(function(position) {
-        http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
-
-        $.getJSON("api.openweathermap.org/data/2.5/forcast?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "&APPID={d6dad265455539cfaba673a7f3cf6bc8}}", function(data) {
+        $.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=d6dad265455539cfaba673a7f3cf6bc8", function(data) {
+        //$.getJSON("api.openweathermap.org/data/2.5/forcast?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "&APPID={d6dad265455539cfaba673a7f3cf6bc8}}", function(data) {
         //do stuff with data returned as json
           console.log(data[0].name);
         });
