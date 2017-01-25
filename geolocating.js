01b7695cae248e78c68bfe3525d467d1a1287d7f
Karla Aguilar <kn.aguilar7@gmail.com>
2017-01-24 21:23:21 -0500

    bug: add api key for weather app

--- geolocating.js
+++ geolocating.js
@@ -8,7 +8,9 @@ $(document).ready(function() {
     if ("geolocation" in navigator) {
       console.log("GeoLocation available");
       navigator.geolocation.getCurrentPosition(function(position) {
-        $.getJSON("api.openweathermap.org/data/2.5/weather?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "}", function(data) {
+        http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
+
+        $.getJSON("api.openweathermap.org/data/2.5/weather?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "&APPID={d6dad265455539cfaba673a7f3cf6bc8}}", function(data) {
         //do stuff with data returned as json
           console.log(data[0].name);
         });
