5d71905f7e2b58736c6ba3aa7d2d62508d655a6f
Karla Aguilar <kn.aguilar7@gmail.com>
2017-01-24 21:59:08 -0500

    bug: fixed issue with obtaining city object

--- geolocating.js
+++ geolocating.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         $.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=d6dad265455539cfaba673a7f3cf6bc8", function(data) {
         //$.getJSON("api.openweathermap.org/data/2.5/forcast?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "&APPID={d6dad265455539cfaba673a7f3cf6bc8}}", function(data) {
         //do stuff with data returned as json
-          console.log(data[0].name);
+          console.log(data.city.name); //moscow
         });
         $.ajaxSetup({ cache: false });
       });
