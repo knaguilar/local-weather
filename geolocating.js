1b10b78379928ad4de0b77e6a5e5893cbc26ca9f
Karla Aguilar <kn.aguilar7@gmail.com>
2017-01-24 21:36:12 -0500

    bug: url from weather to forcast

--- geolocating.js
+++ geolocating.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       navigator.geolocation.getCurrentPosition(function(position) {
         http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
 
-        $.getJSON("api.openweathermap.org/data/2.5/weather?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "&APPID={d6dad265455539cfaba673a7f3cf6bc8}}", function(data) {
+        $.getJSON("api.openweathermap.org/data/2.5/forcast?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "&APPID={d6dad265455539cfaba673a7f3cf6bc8}}", function(data) {
         //do stuff with data returned as json
           console.log(data[0].name);
         });
