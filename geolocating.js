de4fcdf5d08639e51848a368fa78855837b7a193
Karla Aguilar <kn.aguilar7@gmail.com>
2017-01-29 16:02:07 -0500

    bug: use jsonp to request to api

--- geolocating.js
+++ geolocating.js
@@ -1,5 +1,26 @@
+var weatherData;
+
+/* Forecast.io API Variables (Weather Forecasting) */
+var FORECAST_URL =  "https://api.forecast.io/forecast/";
+var FORECAST_API =  "9e33c0fe31a85de97a958d06533d6a18";  // Your API Key
+var latitude     =  "";  // Your Latitude
+var longitude    =  "";  // Your Longitude
+
 function do_something(lat, long) {
   console.log("lat is " + lat + " and long is " + long);
+}
+
+function fetchWeather() {
+  $.ajax({
+    url: FORECAST_URL + FORECAST_API + '/' + latitude + ',' + longitude + "?units=auto",
+    dataType: "jsonp",
+    success: function (data) { weatherData = data;  /* Store our newly aquired weather data */ }
+  });
+
+  console.log("Success: data is " + data);
+
+  // Fetch the weather every fifteen minutes
+  setTimeout(function() { fetchWeather();  }, 900000);
 }
 
 $(document).ready(function() {
@@ -7,12 +28,9 @@ $(document).ready(function() {
     if ("geolocation" in navigator) {
       console.log("GeoLocation available");
       navigator.geolocation.getCurrentPosition(function(position) {
-        var url = "https://api.darksky.net/forecast/9e33c0fe31a85de97a958d06533d6a18/" + position.coords.latitude + "," + position.coords.longitude;
-        $.getJSON(url, function(data) {
-        //do stuff with data returned as json
-          console.log(data); //moscow
-        });
-        $.ajaxSetup({ cache: false });
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        fetchWeather();
       });
     /* geolocation is available */
     } else {
