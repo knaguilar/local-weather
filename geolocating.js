c79aece6f78b707a6af7c8cf8568f4fa3a79ef65
Karla Aguilar <kn.aguilar7@gmail.com>
2017-01-29 17:03:29 -0500

    feat: obtain weather temp from darksky api

--- geolocating.js
+++ geolocating.js
@@ -1,10 +1,10 @@
 var weatherData;
 
 /* Forecast.io API Variables (Weather Forecasting) */
-var FORECAST_URL =  "https://api.forecast.io/forecast/";
+var FORECAST_URL =  "https://api.darksky.net/forecast/";
 var FORECAST_API =  "9e33c0fe31a85de97a958d06533d6a18";  // Your API Key
-var latitude     =  "";  // Your Latitude
-var longitude    =  "";  // Your Longitude
+var latitude     =  "42.3601";  // Your Latitude
+var longitude    =  "-71.0589";  // Your Longitude
 
 function do_something(lat, long) {
   console.log("lat is " + lat + " and long is " + long);
@@ -12,12 +12,13 @@ function do_something(lat, long) {
 
 function fetchWeather() {
   $.ajax({
-    url: FORECAST_URL + FORECAST_API + '/' + latitude + ',' + longitude + "?units=auto",
+    url: FORECAST_URL + FORECAST_API + '/' + latitude + ',' + longitude,
     dataType: "jsonp",
-    success: function (data) { weatherData = data;  /* Store our newly aquired weather data */ }
+    success: function (data) {
+      weatherData = data;  /* Store our newly aquired weather data */
+      console.log("Success: data is " + weatherData.currently.temperature);
+    }
   });
-
-  console.log("Success: data is " + data);
 
   // Fetch the weather every fifteen minutes
   setTimeout(function() { fetchWeather();  }, 900000);
@@ -31,6 +32,7 @@ $(document).ready(function() {
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         fetchWeather();
+        do_something(latitude, longitude);
       });
     /* geolocation is available */
     } else {
