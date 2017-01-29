08267f04dc597d3666b85a0327295031bd4ccca4
Karla Aguilar <kn.aguilar7@gmail.com>
2017-01-29 15:01:04 -0500

    feat: use darksky api

--- geolocating.js
+++ geolocating.js
@@ -2,16 +2,14 @@ function do_something(lat, long) {
   console.log("lat is " + lat + " and long is " + long);
 }
 
-
 $(document).ready(function() {
   $('.button').on("click", function() {
     if ("geolocation" in navigator) {
       console.log("GeoLocation available");
       navigator.geolocation.getCurrentPosition(function(position) {
-        $.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=d6dad265455539cfaba673a7f3cf6bc8", function(data) {
-        //$.getJSON("api.openweathermap.org/data/2.5/forcast?lat={" + position.coords.latitude + "}&lon={"+ position.coords.longitude + "&APPID={d6dad265455539cfaba673a7f3cf6bc8}}", function(data) {
+        $.getJSON("https://api.darksky.net/forecast/9e33c0fe31a85de97a958d06533d6a18/" + position.coords.latitude + ","+ position.coords.longitude + ", function(data) {
         //do stuff with data returned as json
-          console.log(data.city.name); //moscow
+          console.log(data); //moscow
         });
         $.ajaxSetup({ cache: false });
       });
